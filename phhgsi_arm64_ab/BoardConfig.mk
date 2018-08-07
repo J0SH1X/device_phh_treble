976272beb248eeed195c8347806deb560d6deacf
J0SH1X <aljoshua.hell@gmail.com>
2018-08-08 00:37:45 +0200

    add judyln.mk

--- phhgsi_arm64_ab/BoardConfig.mk
+++ phhgsi_arm64_ab/BoardConfig.mk
@@ -1,4 +1,5 @@
 include build/make/target/board/generic_arm64_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
+include device/phh/treble/judyln.mk
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
